48908ddca40eb4e8e0cfa51fafcd0585066337ed
bannavong <bannavong.sa239717@sis.hust.edu.vn>
2026-05-26 14:03:01 +0700

    modify(format) update format file
    Signed-off-by: bannavong <bannavong.sa239717@sis.hust.edu.vn>

--- lib/features/util/data/models/language_option.dart
+++ lib/features/util/data/models/language_option.dart
@@ -5,10 +5,8 @@ part 'language_option.g.dart';
 
 @freezed
 class LanguageOption with _$LanguageOption {
-  const factory LanguageOption({
-    required String code,
-    required String name,
-  }) = _LanguageOption;
+  const factory LanguageOption({required String code, required String name}) =
+      _LanguageOption;
 
   factory LanguageOption.fromJson(Map<String, dynamic> json) =>
       _$LanguageOptionFromJson(json);
--- lib/features/util/data/models/util_feedback_response.dart
+++ lib/features/util/data/models/util_feedback_response.dart
@@ -5,9 +5,8 @@ part 'util_feedback_response.g.dart';
 
 @freezed
 class UtilFeedbackResponse with _$UtilFeedbackResponse {
-  const factory UtilFeedbackResponse({
-    required String message,
-  }) = _UtilFeedbackResponse;
+  const factory UtilFeedbackResponse({required String message}) =
+      _UtilFeedbackResponse;
 
   factory UtilFeedbackResponse.fromJson(Map<String, dynamic> json) =>
       _$UtilFeedbackResponseFromJson(json);
